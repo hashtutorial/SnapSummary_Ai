5971b57640def7a14d257098a15a526edb644ef8
hashtutorial <l227753@lhr.nu.edu.pk>
2025-07-14 04:24:03 +0500

    Delete SnapSummary_Ai/Nexium_Hashir_assign2 directory

--- SnapSummary_Ai/Nexium_Hashir_assign2/next.config.js
+++
@@ -1,9 +0,0 @@
-// next.config.js to skip linting errors during builds
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-};
-
-module.exports = nextConfig;
